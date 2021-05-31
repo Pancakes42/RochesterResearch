fc14c98620b62981927aafd9120d900b79f9d0f1
Pancakes42 <36553713+Pancakes42@users.noreply.github.com>
2021-05-30 20:17:34 -0400

    Created Base + Billboard
    Basic walking and firstperson. Floating text billboard

---
+++ rrproj/Assets/Scripts/TextDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDisplay : MonoBehaviour
+{
+    GameObject GameController;
+    GameObject PlayerObject;
+    Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController = GameObject.FindGameObjectWithTag("GameController");
+        PlayerObject = GameController.GetComponent<GameController>().PlayerChar;
+        playerCamera = PlayerObject.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.LookAt(transform.position + playerCamera.transform.rotation * Vector3.forward,
+            playerCamera.transform.rotation * Vector3.up);
+
+    }
+}
